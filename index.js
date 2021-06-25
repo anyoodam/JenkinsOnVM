928b14783793b599c90d64c20942066d2ccc2f85
anyoodam <86398814+anyoodam@users.noreply.github.com>
2021-06-25 17:44:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like fsdfsdfsedfsedfsdfsdf!');
+    res.send('I like 1111111111111111111111111!');
 });
 
 app.get('/start', function(req, res){
